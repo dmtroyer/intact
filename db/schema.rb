5372f38e3dd205c952f576756d74001ebb473eb3
David Troyer <dmtroyer@gmail.com>
2016-05-24 11:47:21 -0400

    oops forgot the schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160523221229) do
+ActiveRecord::Schema.define(version: 20160524153028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "hashed_strings", force: :cascade do |t|
+    t.string   "original"
+    t.string   "hashed"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "hashed_strings", ["user_id"], name: "index_hashed_strings_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -34,4 +44,5 @@ ActiveRecord::Schema.define(version: 20160523221229) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "hashed_strings", "users"
 end
