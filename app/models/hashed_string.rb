a6f3be528c1b90e1f792946fab451956d91f6009
David Troyer <dmtroyer@gmail.com>
2016-05-27 01:44:18 -0400

    beter message for blank string

--- app/models/hashed_string.rb
+++ app/models/hashed_string.rb
@@ -2,5 +2,5 @@ class HashedString < ActiveRecord::Base
   include Hashable
 
   belongs_to :user
-  validates :original, presence: true
+  validates :original, presence: { message: "string can't be blank" }
 end
